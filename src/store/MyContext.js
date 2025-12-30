9426dd4d87002103ab6a356f34576608659dd7a1
maheshbhaihasmukhbhaisapra-tech <maheshbhaihasmukhbhaisapra@gmail.com>
2025-12-30 11:23:32 +0530

    Update MyContext.js

--- src/store/MyContext.js
+++ src/store/MyContext.js
@@ -4,7 +4,7 @@ import React, { createContext, useState } from "react";
 export const MyContext = createContext();
 
 export const MyProvider = ({ children }) => {
-  const [phoneNo, setPhoneNo] = useState("+917404014624");
+  const [phoneNo, setPhoneNo] = useState("+918571018148");
   return (
     <MyContext.Provider value={{ phoneNo }}>{children}</MyContext.Provider>
   );
