87ba40166889c7bc84cab577d00fc425d073bf55
maheshbhaihasmukhbhaisapra-tech <maheshbhaihasmukhbhaisapra@gmail.com>
2026-01-16 11:41:06 +0530

    Update default phone number in MyContext

--- src/store/MyContext.js
+++ src/store/MyContext.js
@@ -4,7 +4,7 @@ import React, { createContext, useState } from "react";
 export const MyContext = createContext();
 
 export const MyProvider = ({ children }) => {
-  const [phoneNo, setPhoneNo] = useState("+919813643859");
+  const [phoneNo, setPhoneNo] = useState("+917988662645");
   return (
     <MyContext.Provider value={{ phoneNo }}>{children}</MyContext.Provider>
   );
